e2030515e79d0c81a37856191fca1798ec091a33
Peter Ma <nyceane@gmail.com>
2019-11-22 19:25:14 -0800

    added icons for testing

--- data-example/src/main/java/com/bose/wearable/sample/FallFragment.java
+++ data-example/src/main/java/com/bose/wearable/sample/FallFragment.java
@@ -1,15 +1,20 @@
 package com.bose.wearable.sample;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.speech.tts.TextToSpeech;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bose.wearable.sample.viewmodels.GestureEvent;
 import com.bose.wearable.sample.viewmodels.SessionViewModel;
 import com.bose.wearable.sensordata.SensorValue;
 import com.bose.wearable.services.wearablesensor.SamplePeriod;
@@ -36,12 +41,23 @@ public class FallFragment extends Fragment {
     private int laycount = 0;
     TextToSpeech t1;
 
+    ImageView iconPerson;
+    ImageView iconStatus;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull final LayoutInflater inflater,
                              @Nullable final ViewGroup container,
                              @Nullable final Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_display, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        iconPerson = view.findViewById(R.id.iconPerson);
+        iconStatus = view.findViewById(R.id.iconStatus);
     }
 
     @Override
@@ -52,9 +68,6 @@ public class FallFragment extends Fragment {
         mViewModel.accelerometerData()
                 .observe(this, this::onAccSensorData);
 
-        mViewModel.sensorSamplePeriod(SamplePeriod._80_MS);
-        mViewModel.enableSensor(SensorType.ACCELEROMETER, SamplePeriod._80_MS.milliseconds());
-
         t1=new TextToSpeech(this.getContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
@@ -63,6 +76,38 @@ public class FallFragment extends Fragment {
                 }
             }
         });
+
+        mViewModel.sensorSamplePeriod(SamplePeriod._80_MS);
+        mViewModel.enableSensor(SensorType.ACCELEROMETER, SamplePeriod._80_MS.milliseconds());
+
+        mViewModel.gestureEvents()
+                .observe(this, event -> {
+                    final GestureEvent gestureEvent = event.get();
+                    if (gestureEvent != null && gestureEvent.gestureData().type().name().equalsIgnoreCase("DOUBLE_TAP")) {
+                        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this.getContext());
+                        String phoneNumber = preferences.getString(this.getContext().getString(R.string.contact), "");
+                        if(!phoneNumber.equalsIgnoreCase(""))
+                        {
+                            t1.speak("Calling Guardian", TextToSpeech.QUEUE_FLUSH, null, "txtFall");
+
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+                                    startActivity(intent);
+                                }
+
+                            }, 1500); // 5000ms delay
+                        }
+                    }
+                    else if(gestureEvent != null && gestureEvent.gestureData().type().name().equalsIgnoreCase("NEGATIVE")) {
+
+                    }
+                });
+
+
     }
 
 
@@ -96,6 +141,8 @@ public class FallFragment extends Fragment {
                             fallDetected = true;
                             Log.e("fall", "true fall detected");
                             t1.speak("Fall detected, please see if you can move", TextToSpeech.QUEUE_FLUSH, null, "txtFall");
+                            iconPerson.setImageResource(R.drawable.fell);
+                            iconStatus.setImageResource(R.drawable.warning);
                         }
                     }
                 }
@@ -120,7 +167,8 @@ public class FallFragment extends Fragment {
                             laycount = 0;
                             fallDetected = false;
                             t1.speak("It seems you are ok, you can double tap to make emergency phone call", TextToSpeech.QUEUE_FLUSH, null, "txtFall");
-
+                            iconPerson.setImageResource(R.drawable.standing);
+                            iconStatus.setImageResource(R.drawable.check);
                         }
 
                     }
@@ -140,6 +188,10 @@ public class FallFragment extends Fragment {
         t1.speak("You are not moving, sending a text to your emergency contact", TextToSpeech.QUEUE_FLUSH, null, "txtFall");
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this.getContext());
         String smsNumber = preferences.getString(this.getContext().getString(R.string.contact), "");
+        movecount = 0;
+        laycount = 0;
+        fallDetected = false;
+
         if(!smsNumber.equalsIgnoreCase(""))
         {
             String helpMessage = "I have fallen and need help, sent by Senior Guardian Frames on Android";
@@ -152,12 +204,28 @@ public class FallFragment extends Fragment {
             Toast.makeText(this.getContext(), "Your contact has been notified",
                     Toast.LENGTH_LONG).show();
         }
-        movecount = 0;
-        laycount = 0;
-        fallDetected = false;
+
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                iconPerson.setImageResource(R.drawable.standing);
+                iconStatus.setImageResource(R.drawable.check);
+            }
+
+        }, 5000); // 5000ms delay
 
     }
 
 
+    @Override
+    public void onDestroyView()
+    {
+        super.onDestroyView();
+        t1.shutdown();
+    }
 
 }
